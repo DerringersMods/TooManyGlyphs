9a71721ceaacf9dbac6db35874a1fceabcc65be7
DerringersMods <m4jgyvmc9k@liamekaens.com>
2021-12-29 16:01:47 -0800

    clean up some unused imports

--- src/main/java/io/github/derringersmods/toomanyglyphs/common/mixin/RuneBlockFilterMixin.java
+++ src/main/java/io/github/derringersmods/toomanyglyphs/common/mixin/RuneBlockFilterMixin.java
@@ -4,20 +4,14 @@ import com.hollingsworth.arsnouveau.common.block.RuneBlock;
 import com.hollingsworth.arsnouveau.common.block.tile.RuneTile;
 import io.github.derringersmods.toomanyglyphs.api.FilterUtil;
 import io.github.derringersmods.toomanyglyphs.common.glyphs.EffectFilterAny;
-import net.minecraft.block.BlockState;
 import net.minecraft.entity.Entity;
-import net.minecraft.util.math.BlockPos;
 import net.minecraft.util.math.EntityRayTraceResult;
-import net.minecraft.world.World;
 import org.objectweb.asm.Opcodes;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
-import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.Redirect;
-import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
-import org.spongepowered.asm.mixin.injection.callback.LocalCapture;
 
-import java.util.Random;
+import javax.annotation.Nonnull;
 
 @Mixin(RuneBlock.class)
 public class RuneBlockFilterMixin {
@@ -25,7 +19,7 @@ public class RuneBlockFilterMixin {
               at = @At(value = "FIELD",
                        target = "Lcom/hollingsworth/arsnouveau/common/block/tile/RuneTile;touchedEntity:Lnet/minecraft/entity/Entity;",
                        opcode = Opcodes.PUTFIELD))
-    private void entityInsideFilterCheck(RuneTile instance, Entity value) {
+    private void entityInsideFilterCheck(@Nonnull RuneTile instance, Entity value) {
         if (instance.touchedEntity != null) return;
         if (!FilterUtil.getTargetFilter(instance.spell, EffectFilterAny.INSTANCE).matches(new EntityRayTraceResult(value))) return;
         instance.touchedEntity = value;
