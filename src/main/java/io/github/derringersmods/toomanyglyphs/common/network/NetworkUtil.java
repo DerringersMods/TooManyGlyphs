470dbc50828985ce7a1516cbe6e7f96b9c316a11
DerringersMods <m4jgyvmc9k@liamekaens.com>
2022-01-11 17:17:12 -0800

    Ray FX cleanup

--- src/main/java/io/github/derringersmods/toomanyglyphs/common/network/NetworkUtil.java
+++ src/main/java/io/github/derringersmods/toomanyglyphs/common/network/NetworkUtil.java
@@ -3,30 +3,53 @@ package io.github.derringersmods.toomanyglyphs.common.network;
 import com.hollingsworth.arsnouveau.client.particle.ParticleColor;
 import net.minecraft.network.PacketBuffer;
 import net.minecraft.util.math.vector.Vector3d;
+import net.minecraftforge.fml.network.NetworkEvent;
+
+import javax.annotation.Nonnull;
+import java.util.concurrent.ConcurrentHashMap;
 
 public class NetworkUtil {
-    public static void encode(PacketBuffer buf, Vector3d item)
+    public static ConcurrentHashMap<Class, AbstractPacketHandler> clientHandlers = new ConcurrentHashMap<>();
+
+    public static <MSG> AbstractPacketHandler<MSG> getClientHandlerFor(Class<MSG> packetType) {
+            return (AbstractPacketHandler<MSG>) clientHandlers.computeIfAbsent(packetType, t -> {
+                try {
+                    return (AbstractPacketHandler<MSG>) Class.forName(t.getTypeName() + "$ClientHandler").newInstance();
+                } catch (InstantiationException | IllegalAccessException | ClassNotFoundException | ClassCastException e) {
+                    e.printStackTrace();
+                }
+                return NullPacketHandler.INSTANCE;
+            });
+    }
+
+    public static class NullPacketHandler<MSG> extends AbstractPacketHandler<MSG>
     {
+        public static final NullPacketHandler<Object> INSTANCE = new NullPacketHandler<>();
+
+        @Override
+        public void accept(MSG msg, NetworkEvent.Context context) { }
+    }
+
+    public static void encode(@Nonnull PacketBuffer buf, @Nonnull Vector3d item) {
         buf.writeDouble(item.x);
         buf.writeDouble(item.y);
         buf.writeDouble(item.z);
     }
 
-    public static Vector3d decodeVector3d(PacketBuffer buf)
-    {
+    @Nonnull
+    public static Vector3d decodeVector3d(@Nonnull PacketBuffer buf) {
         double x = buf.readDouble();
         double y = buf.readDouble();
         double z = buf.readDouble();
         return new Vector3d(x, y, z);
     }
 
-    public static void encode(PacketBuffer buf, ParticleColor.IntWrapper item)
-    {
+    public static void encode(@Nonnull PacketBuffer buf, @Nonnull ParticleColor.IntWrapper item) {
         buf.writeInt((item.r << 16) | (item.g << 8) | item.b);
     }
 
-    public static ParticleColor.IntWrapper decodeParticleColorIntWrapper(PacketBuffer buf)
-    {
+    @Nonnull
+    public static ParticleColor.IntWrapper decodeParticleColorIntWrapper(@Nonnull PacketBuffer buf) {
         int rgb = buf.readInt();
         return new ParticleColor.IntWrapper((rgb >> 16) & 0xFF, (rgb >> 8) & 0xFF, rgb & 0xFF);
     }
--- src/main/java/io/github/derringersmods/toomanyglyphs/common/network/PacketRayEffect.java
+++ src/main/java/io/github/derringersmods/toomanyglyphs/common/network/PacketRayEffect.java
@@ -1,14 +1,24 @@
 package io.github.derringersmods.toomanyglyphs.common.network;
 
+import com.hollingsworth.arsnouveau.api.spell.SpellContext;
 import com.hollingsworth.arsnouveau.client.particle.GlowParticleData;
 import com.hollingsworth.arsnouveau.client.particle.ParticleColor;
+import io.github.derringersmods.toomanyglyphs.init.TooManyGlyphsNetworking;
 import net.minecraft.client.Minecraft;
+import net.minecraft.client.entity.player.ClientPlayerEntity;
 import net.minecraft.client.world.ClientWorld;
 import net.minecraft.network.PacketBuffer;
+import net.minecraft.util.math.BlockPos;
+import net.minecraft.util.math.ChunkPos;
 import net.minecraft.util.math.MathHelper;
 import net.minecraft.util.math.vector.Vector3d;
+import net.minecraft.world.World;
+import net.minecraft.world.server.ServerWorld;
 import net.minecraftforge.fml.LogicalSide;
 import net.minecraftforge.fml.network.NetworkEvent;
+import net.minecraftforge.fml.network.PacketDistributor;
+
+import javax.annotation.Nonnull;
 import java.util.function.Supplier;
 
 public class PacketRayEffect {
@@ -38,28 +48,53 @@ public class PacketRayEffect {
     public static void handle(final PacketRayEffect msg, Supplier<NetworkEvent.Context> contextSupplier) {
         final NetworkEvent.Context ctx = contextSupplier.get();
         if (ctx.getDirection().getReceptionSide() == LogicalSide.CLIENT) {
-            ctx.enqueueWork(new Runnable() {
-                @Override
-                public void run() {
-                    Minecraft mc = Minecraft.getInstance();
-                    ClientWorld level = mc.level;
-
-                    double distance = msg.from.distanceTo(msg.to);
-                    for (double d = 0.0; d < distance; d += 1/16.0) {
-                        double fractionalDistance = d / distance;
-                        level.addParticle(
-                                GlowParticleData.createData(msg.colors.toParticleColor()),
-                                MathHelper.lerp(fractionalDistance, msg.from.x, msg.to.x),
-                                MathHelper.lerp(fractionalDistance, msg.from.y, msg.to.y),
-                                MathHelper.lerp(fractionalDistance, msg.from.z, msg.to.z),
-                                (level.random.nextFloat() - 0.5) * 0.1,
-                                (level.random.nextFloat() - 0.5) * 0.1,
-                                (level.random.nextFloat() - 0.5) * 0.1);
-                    }
-                }
-            });
+            ctx.enqueueWork(() -> NetworkUtil.getClientHandlerFor(PacketRayEffect.class).accept(msg, ctx));
         }
         ctx.setPacketHandled(true);
     }
 
+    public static class ClientHandler extends AbstractPacketHandler<PacketRayEffect> {
+        public void accept(PacketRayEffect msg, NetworkEvent.Context context) {
+            Minecraft mc = Minecraft.getInstance();
+            ClientWorld level = mc.level;
+            if (level == null) return;
+            ClientPlayerEntity player = mc.player;
+            if (player == null) return;
+
+            double distance = msg.from.distanceTo(msg.to);
+            double start = 0.0, increment = 1.0/16.0;
+            if (player.position().distanceToSqr(msg.from) < 4.0 && msg.to.subtract(msg.from).normalize().dot(player.getViewVector(1f)) > MathHelper.SQRT_OF_TWO/2) {
+                start = Math.min(2.0, distance / 2.0);
+                increment = 1.0 / 8.0;
+            }
+            for (double d = start; d < distance; d += increment) {
+                double fractionalDistance = d / distance;
+                double speedCoefficient = MathHelper.lerp(fractionalDistance, 0.2, 0.001);
+                level.addParticle(
+                        GlowParticleData.createData(msg.colors.toParticleColor()),
+                        MathHelper.lerp(fractionalDistance, msg.from.x, msg.to.x),
+                        MathHelper.lerp(fractionalDistance, msg.from.y, msg.to.y),
+                        MathHelper.lerp(fractionalDistance, msg.from.z, msg.to.z),
+                        (level.random.nextFloat() - 0.5) * speedCoefficient,
+                        (level.random.nextFloat() - 0.5) * speedCoefficient,
+                        (level.random.nextFloat() - 0.5) * speedCoefficient);
+            }
+        }
+
+    }
+
+    public static void send(@Nonnull World world, @Nonnull SpellContext spellContext, @Nonnull Vector3d fromPoint, @Nonnull Vector3d hitPoint) {
+        Vector3d midpoint = fromPoint.add(hitPoint).scale(0.5);
+        double radius = 64.0 + fromPoint.distanceTo(midpoint);
+        double radiusSqr = radius * radius;
+
+        if (world instanceof ServerWorld)
+        {
+            PacketRayEffect fx = new PacketRayEffect(fromPoint, hitPoint, spellContext.colors);
+            ServerWorld serverWorld = (ServerWorld) world;
+            serverWorld.getChunkSource().chunkMap.getPlayers(new ChunkPos(new BlockPos(midpoint)), false)
+                    .filter(p -> p.distanceToSqr(midpoint) <= radiusSqr)
+                    .forEach(p -> TooManyGlyphsNetworking.fxChannel.send(PacketDistributor.PLAYER.with(() -> p), fx));
+        }
+    }
 }
